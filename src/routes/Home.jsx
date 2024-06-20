af21fb1d63eb99f88f9d595664b86a1b3dbbf36c
Batissta <francinaldofilho05@gmail.com>
2024-06-20 11:33:30 -0300

    Feat - first part of the main at the homepage

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,57 +1,34 @@
 import React from "react";
+import logoLeV from "../assets/logos/logo_l&v.png";
+import logoPrada from "../assets/logos/logo_prada.png";
+import logoRolex from "../assets/logos/logo_rolex.png";
+import logoVersace from "../assets/logos/logo_versace.png";
+import "../css/routes/Home.css";
 
 const Home = () => {
   return (
     <>
-      <section
-        className="homeImagens"
-        style={{
-          display: "grid",
-          gridTemplateColumns: "repeat(3, 1fr)",
-          maxWidth: "1000px",
-          margin: "40px auto",
-          gap: "2rem",
-        }}
-      >
+      <main className="homeMain container">
         <img
           src="https://saint-laurent.dam.kering.com/m/4316411df4b63805/original/freetemplate_hailey_bieber.jpg"
           alt=""
-          style={{ width: "340px", height: "600px", objectFit: "cover" }}
         />
         <img
           src="https://saint-laurent.dam.kering.com/m/4316411df4b63805/original/freetemplate_hailey_bieber.jpg"
           alt=""
-          style={{ width: "340px", height: "600px", objectFit: "cover" }}
         />
         <img
           src="https://saint-laurent.dam.kering.com/m/4316411df4b63805/original/freetemplate_hailey_bieber.jpg"
           alt=""
-          style={{ width: "340px", height: "600px", objectFit: "cover" }}
         />
-      </section>
+      </main>
 
-      <section
-        className="homeImagem"
-        style={{
-          display: "grid",
-          gridTemplateColumns: "1fr",
-          maxWidth: "1000px",
-          margin: "400px auto",
-          gap: "2rem",
-        }}
-      >
-        <img
-          src="https://saint-laurent.dam.kering.com/m/1a58645a1d1615f7/original/freetemplate_hailey_bieber.jpg"
-          alt=""
-          style={{
-            width: "calc(100% - 100px)",
-            height: "100%",
-            objectFit: "cover",
-            margin: "0 auto",
-          }}
-        />
+      <section className="marcas">
+        <img src={logoLeV} alt="" />
+        <img src={logoPrada} alt="" />
+        <img src={logoRolex} alt="" />
+        <img src={logoVersace} alt="" />
       </section>
-      <h1>Home</h1>
     </>
   );
 };
