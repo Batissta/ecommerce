f7b09bc994cd488af5018be86d3713a0ef31b3ec
Batissta <francinaldofilho05@gmail.com>
2024-06-26 10:42:36 -0300

    feat - errorPage and main

--- src/routes/ErrorPage.jsx
+++ src/routes/ErrorPage.jsx
@@ -1,13 +1,16 @@
-import React from 'react';
-import { Link, useRouteError } from 'react-router-dom';
+import React from "react";
+import { Link, useRouteError } from "react-router-dom";
 
 const ErrorPage = () => {
   const error = useRouteError();
+  const hrefSplited = window.location.href.split("/");
+  const href = hrefSplited[hrefSplited.length - 1];
+
   return (
     <>
       <h1>Ops...</h1>
       <p>Perdão, um erro inesperado aconteceu</p>
-      <p>{error.statusText || error.message}</p>
+      <p>{href === "desenvolvimento" && <p>Seção em desenvolvimento...</p>}</p>
     </>
   );
 };
